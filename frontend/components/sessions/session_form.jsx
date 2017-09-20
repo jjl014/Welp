4a79fb9455a555f1bdd5e90dcea460b8a46034cc
Jimmy Li <jimmyli921@gmail.com>
2017-09-20 16:03:42 -0700

    Updated CSS and restructered Session Form

--- frontend/components/sessions/session_form.jsx
+++ frontend/components/sessions/session_form.jsx
@@ -60,44 +60,56 @@ export default class SessionForm extends React.Component {
   render() {
     let display;
     let button;
+    let message;
+    let sub1;
+    let sub2;
     if(this.props.formType === 'signup') {
       button = 'Sign Up';
       display = (
         <input onChange={this.handleChange('email')} type="email" value={this.state.email} placeholder="Email"></input>
       );
+      message = "Sign Up for Welp";
+      sub1 = null;
+      sub2 = (<p className="subtle-text">Already on Yelp? <Link to='/login'>Log in</Link></p>);
     } else {
       button = 'Log In';
       display = null;
+      message = "Log In to Welp";
+      sub1 = (<p className="subheader">New to Welp? <Link to='/signup'>Sign Up</Link></p>);
+      sub2 = null;
     }
     return (
       <div className="session-container">
         <header className="session-header">
           <Link to="/">Welp</Link>
         </header>
-        <div className="session-form-container">
-          <div className="column-1-2">
-            <div className="session-form-container-left">
-              {this.renderErrors()}
-              <form className="session-form">
-                <h2>Log in to Yelp</h2>
-                <p className="">New to Yelp? <Link to='/signup'>Sign up</Link></p>
-                <input onChange={this.handleChange('username')}
-                  type="text"
-                  value={this.state.username}
-                  placeholder="Username"></input>
-                <input onChange={this.handleChange('password')}
-                  type="password"
-                  value={this.state.password}
-                  placeholder="Password"></input>
-                {display}
-                <button onClick={this.handleSubmit()}>{button}</button>
-                <button onClick={this.handleDemoLogin()}>Demo Login</button>
-              </form>
+        <div className="session-body">
+          <div className="session-form-container">
+            <div className="column-1-2">
+              <div className="session-form-container-left">
+                {this.renderErrors()}
+                <h2>{message}</h2>
+                {sub1}
+                <form className="session-form">
+                  <input onChange={this.handleChange('username')}
+                    type="text"
+                    value={this.state.username}
+                    placeholder="Username"></input>
+                  <input onChange={this.handleChange('password')}
+                    type="password"
+                    value={this.state.password}
+                    placeholder="Password"></input>
+                  {display}
+                  <button className="btn-primary" onClick={this.handleSubmit()}>{button}</button>
+                  <button className="btn-primary" onClick={this.handleDemoLogin()}>Demo Login</button>
+                </form>
+                {sub2}
+              </div>
             </div>
-          </div>
-          <div className="column-1-2">
-            <div className="session-form-container-right">
-              <img src="https://res.cloudinary.com/jun/image/upload/v1505933693/signup_illustration_qw2im7.png" alt="Welcome Image"></img>
+            <div className="column-1-2">
+              <div className="session-form-container-right">
+                <img src="https://res.cloudinary.com/jun/image/upload/v1505933693/signup_illustration_qw2im7.png" alt="Welcome Image"></img>
+              </div>
             </div>
           </div>
         </div>
