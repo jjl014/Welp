035c435981007934f7f763e1a35eee9a44edf97b
Jimmy Li <jimmyli921@gmail.com>
2017-09-24 16:39:29 -0700

    removed devtools

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 export const configureStore = (preloadedState ={}) => (
-  createStore(RootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk, logger)))
+  createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
 );
