0e5bdcf319ff6c82feae5a33e40787d2054f9ba4
Jimmy Li <jimmyli921@gmail.com>
2017-10-30 15:15:07 -0700

    Change API key

--- frontend/util/map_api_util.js
+++ frontend/util/map_api_util.js
@@ -2,7 +2,7 @@ export const getGeoFromAddress = ({address, city, state, zipcode}) => {
   const query = [address, city, state, zipcode].join("+");
   return $.ajax({
     method: 'GET',
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=AIzaSyCSX3XPr-HFJG-dNlag9UKwvw2OCzbGz1s`
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${window.google_apis.google_geocode_api_key}`
   });
 };
 
